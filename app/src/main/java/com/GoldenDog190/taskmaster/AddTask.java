b2b36dfed7212a2d9a293ddd420f9c217b212aef
Amelia Waggoner <wildginger@wavecable.com>
2021-04-26 18:43:18 -0700

    added java for all of the form and buttons

--- app/src/main/java/com/GoldenDog190/taskmaster/AddTask.java
+++ app/src/main/java/com/GoldenDog190/taskmaster/AddTask.java
@@ -2,13 +2,34 @@ package com.GoldenDog190.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AddTask extends AppCompatActivity {
+    public static String TAG = "GoldenDog190.main";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+        Button button = findViewById(R.id.TaskButton);
+        button.setOnClickListener(view -> {
+            Log.i(TAG, "this is the add task button");
+            ((TextView)findViewById(R.id.TaskButton)).setText("Submitted!");
+
+            String taskTitle = ((EditText)findViewById(R.id.editTextTextTaskTitle)).getText().toString();
+
+            String taskDescription = ((EditText)findViewById(R.id.editTextTextTaskDescription)).getText().toString();
+
+            Intent intent = new Intent(AddTask.this, AllTasks.class);
+            intent.putExtra("TaskTitle", taskTitle);
+            intent.putExtra("TaskDescription", taskDescription);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/GoldenDog190/taskmaster/AllTasks.java
+++ app/src/main/java/com/GoldenDog190/taskmaster/AllTasks.java
@@ -1,20 +1,26 @@
 package com.GoldenDog190.taskmaster;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AllTasks extends AppCompatActivity {
 
-
+    public static String TAG = "GoldenDog190.main";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_tasks);
 
-
+    Button goBackButton = findViewById(R.id.backButton);
+    goBackButton.setOnClickListener(view -> {
+        Intent goBackButtonIntent = new Intent(AllTasks.this, AddTask.class);
+        startActivity(goBackButtonIntent);
+    });
 
 
     }
